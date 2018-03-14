6a7876eaa55298b0f171310b466fdc3d2035ceec
Vinicius Matheus <viniciusmsos@gmail.com>
2018-03-14 13:18:14 -0300

    implementação inicial movimento de player e câmera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public Transform playerTransform;
+
+	private float horizontalMovement;
+
+	void LateUpdate ()
+	{
+		horizontalMovement = Input.GetAxis ("Horizontal");
+
+		if (horizontalMovement < 0) {
+			return;
+		}
+
+		if (transform.position.x < playerTransform.position.x) {
+			
+		}
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+	public Transform cameraTransform;
+	public float movementSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
+	private float horizontalMovement;
+	private float moveFactor;
+
+	void Start ()
+	{
+		moveFactor = movementSpeed / 10;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update ()
+	{
+		horizontalMovement = Input.GetAxis ("Horizontal");
+
+		if (horizontalMovement < 0 && (cameraTransform.position.x - transform.position.x) >= 10) {
+			return;
+		}		
+
+		transform.Translate (Vector3.right * horizontalMovement * moveFactor);
+
+		if (Input.GetAxisRaw ("Vertical") < 0) {
+			transform.localScale = new Vector3 (32f, 32f, 1);
+		} else {
+			transform.localScale = new Vector3 (32f, 64f, 1);
+		}
 	}
 }
