736571979f89e589424aaf34a185cab08fe0f6cd
Vinicius Matheus <viniciusmsos@gmail.com>
2018-03-14 16:50:55 -0300

    Implementação inicial do pulo

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,28 +2,35 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+	public LayerMask groundLayer;
 	public Transform cameraTransform;
 	public float movementSpeed;
 	public float sneakRelativeSpeed;
+	public float jumpForce;
 
+	private Rigidbody2D rB2d;
 	private float horizontalMovement;
 	private float moveFactor;
 	private bool isSneaking = false;
 
 	void Start ()
 	{
+		rB2d = GetComponent<Rigidbody2D> ();
 		moveFactor = movementSpeed / 10;
 	}
 
 	void Update ()
 	{
 		horizontalMovement = Input.GetAxis ("Horizontal");
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.down, 0.1f, groundLayer);
 
-		if (horizontalMovement < 0 && (cameraTransform.position.x - transform.position.x) >= 10) {
+		//quando o player tenta voltar no cenário
+		if (horizontalMovement < 0 && (cameraTransform.position.x - transform.position.x) >= 9) {
 			return;
 		}		
 
-		if (Input.GetAxisRaw ("Vertical") < 0) {
+		//quando o player se esconde/se movimenta furtivamente
+		if (Input.GetAxisRaw ("Vertical") < 0 && hit.collider != null) {
 			isSneaking = true;
 			transform.localScale = new Vector3 (32f, 32f, 1);
 		} else {
@@ -35,6 +42,16 @@ public class PlayerMovement : MonoBehaviour
 			transform.Translate (Vector3.right * horizontalMovement * moveFactor * sneakRelativeSpeed);
 		} else {
 			transform.Translate (Vector3.right * horizontalMovement * moveFactor);
+		}
+
+		//quando o player pula
+		if (Input.GetButtonDown ("Jump") && !isSneaking) {
+			if (hit.collider != null) {
+				Debug.Log ("pulando");
+				rB2d.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+			} else {
+				Debug.Log ("hit null");
+			}
 		}
 	}
 }
