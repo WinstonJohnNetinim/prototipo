53186cf41becaa3c293668fd1c8eef51ee4c5833
Vinicius Matheus <viniciusmsos@gmail.com>
2018-03-17 21:00:30 -0300

    Movimento do Player utilizando forças e Rigidbody2D

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,56 +1,94 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
 public class PlayerMovement : MonoBehaviour
 {
 	public LayerMask groundLayer;
 	public Transform cameraTransform;
-	public float movementSpeed;
+	public GameObject leftHand, rightHand;
+	public float horizontalAcceleration;
+	public float maxSpeed;
 	public float sneakRelativeSpeed;
 	public float jumpForce;
 
-	private Rigidbody2D rB2d;
-	private float horizontalMovement;
-	private float moveFactor;
+	private Rigidbody2D rB2D;
+	private SpriteRenderer spriteRenderer;
+	private float horizontalInput, horizontalInputRaw;
+	private float verticalInput, verticalInputRaw;
 	private bool isSneaking = false;
 
-	void Start ()
+	void Start()
 	{
-		rB2d = GetComponent<Rigidbody2D> ();
-		moveFactor = movementSpeed / 10;
+		rB2D = GetComponent<Rigidbody2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 
-	void Update ()
+	void Update()
 	{
-		horizontalMovement = Input.GetAxis ("Horizontal");
-		RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.down, 0.1f, groundLayer);
+		horizontalInput = Input.GetAxis("Horizontal");
+		horizontalInputRaw = Input.GetAxisRaw("Horizontal");
+		verticalInput = Input.GetAxis("Vertical");
+		verticalInputRaw = Input.GetAxisRaw("Vertical");
+
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundLayer);
+
+		//inverte o sprite dependendo do input
+		if (horizontalInputRaw > 0)
+		{
+			spriteRenderer.flipX = false;
+			rightHand.SetActive(true);
+			leftHand.SetActive(false);
+		}
+		if (horizontalInputRaw < 0)
+		{
+			spriteRenderer.flipX = true;
+			rightHand.SetActive(false);
+			leftHand.SetActive(true);
+		}
 
 		//quando o player tenta voltar no cenário
-		if (horizontalMovement < 0 && (cameraTransform.position.x - transform.position.x) >= 9) {
+		if (horizontalInput < 0 && (cameraTransform.position.x - transform.position.x) >= 9)
+		{
+			rB2D.velocity = new Vector2(0f, rB2D.velocity.y);
 			return;
-		}		
+		}
 
 		//quando o player se esconde/se movimenta furtivamente
-		if (Input.GetAxisRaw ("Vertical") < 0 && hit.collider != null) {
+		if (verticalInputRaw < 0 && hit)
+		{
 			isSneaking = true;
-			transform.localScale = new Vector3 (32f, 32f, 1);
-		} else {
+			transform.localScale = new Vector3(1f, 0.5f, 1f);
+		}
+		else
+		{
 			isSneaking = false;
-			transform.localScale = new Vector3 (32f, 64f, 1);
+			transform.localScale = new Vector3(1f, 1f, 1f);
 		}
 
-		if (isSneaking) {
-			transform.Translate (Vector3.right * horizontalMovement * moveFactor * sneakRelativeSpeed);
-		} else {
-			transform.Translate (Vector3.right * horizontalMovement * moveFactor);
+		//quando o player se movimenta, de fato
+		if (isSneaking && Mathf.Abs(rB2D.velocity.x) < maxSpeed * sneakRelativeSpeed)
+		{
+			//transform.Translate (Vector3.right * horizontalInput * sneakRelativeSpeed);
+			rB2D.AddForce(Vector2.right * horizontalAcceleration * sneakRelativeSpeed * horizontalInputRaw * Time.deltaTime);
 		}
+		if (!isSneaking && Mathf.Abs(rB2D.velocity.x) < maxSpeed)
+		{
+			//transform.Translate (Vector3.right * horizontalInput);
+			rB2D.AddForce(Vector2.right * horizontalAcceleration * horizontalInputRaw * Time.deltaTime);
+		}
+
 
 		//quando o player pula
-		if (Input.GetButtonDown ("Jump") && !isSneaking) {
-			if (hit.collider != null) {
-				Debug.Log ("pulando");
-				rB2d.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
-			} else {
-				Debug.Log ("hit null");
+		if (Input.GetButtonDown("Jump") && !isSneaking)
+		{
+			if (hit.collider != null)
+			{
+				Debug.Log("pulando");
+				rB2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+			}
+			else
+			{
+				Debug.Log("hit null");
 			}
 		}
 	}
